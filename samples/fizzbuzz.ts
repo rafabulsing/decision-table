dfd4cb04b9c7e241999beeed88a87ee170d4b6fb
Rafael Bulsing <rafabulsing@gmail.com>
2021-01-12 22:00:36 -0300

    Fix sample

--- samples/fizzbuzz.ts
+++ samples/fizzbuzz.ts
@@ -1,18 +1,15 @@
 import { DecisionTable, any } from '../src/DecisionTable';
 
-const isDivisibleN = (n: number, d: number): boolean => {
-    return n % d === 0
-};
-const isDivisible3 = (n: number): boolean => isDivisibleN(n, 3);
-const isDivisible5 = (n: number): boolean => isDivisibleN(n, 5); 
+const isDivisible3 = (n: number): boolean => n % 3 === 0;
+const isDivisible5 = (n: number): boolean => n % 5 === 0; 
 
-for (let i=1; i<100; ++i) {
+for (let i=1; i<=100; ++i) {
     const print = (str: string) => () => console.log(str);
     DecisionTable.do(
         [isDivisible5(i), isDivisible3(i),                  ],
         [false,           false,           print(`${ i }`)  ],
         [false,           true,            print('fizz')    ],
         [true,            false,           print(`buzz`)    ],
-        [true,            any,             print('fizzbuzz')],
+        [true,            true,            print('fizzbuzz')],
     );
 }
