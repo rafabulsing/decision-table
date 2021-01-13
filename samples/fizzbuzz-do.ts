6105e37fa3d0cef73f2ca1b163c519bdcdbc88d6
Rafael Bulsing <rafabulsing@gmail.com>
2021-01-12 22:12:15 -0300

    Extract print function definition from the loop

--- samples/fizzbuzz-do.ts
+++ samples/fizzbuzz-do.ts
@@ -3,8 +3,9 @@ import { DecisionTable, any } from '../src/DecisionTable';
 const isDivisible3 = (n: number): boolean => n % 3 === 0;
 const isDivisible5 = (n: number): boolean => n % 5 === 0; 
 
+const print = (str: string) => () => console.log(str);
+
 for (let i=1; i<=100; ++i) {
-    const print = (str: string) => () => console.log(str);
     DecisionTable.do(
         [isDivisible5(i), isDivisible3(i),                  ],
         [false,           false,           print(`${ i }`)  ],
