72051c5fa7eaf8bbfd296bea771c00fcc41f050a
Rafael Bulsing <rafabulsing@gmail.com>
2021-01-12 22:57:55 -0300

    Samples formatting

--- samples/fizzbuzz-do.ts
+++ samples/fizzbuzz-do.ts
@@ -1,4 +1,4 @@
-import { DecisionTable, any } from '../src/DecisionTable';
+import { DecisionTable } from '../src/DecisionTable';
 
 const isDivisible3 = (n: number): boolean => n % 3 === 0;
 const isDivisible5 = (n: number): boolean => n % 5 === 0; 
@@ -7,10 +7,10 @@ const print = (str: string) => () => console.log(str);
 
 for (let i=1; i<=100; ++i) {
     DecisionTable.do(
-        [isDivisible5(i), isDivisible3(i),                  ],
-        [false,           false,           print(`${ i }`)  ],
-        [false,           true,            print('fizz')    ],
-        [true,            false,           print('buzz')    ],
-        [true,            true,            print('fizzbuzz')],
+        [ isDivisible5(i), isDivisible3(i),                   ],
+        [ false,           false,           print(`${ i }`)   ],
+        [ false,           true,            print('fizz')     ],
+        [ true,            false,           print('buzz')     ],
+        [ true,            true,            print('fizzbuzz') ],
     );
 }
--- samples/fizzbuzz-set.ts
+++ samples/fizzbuzz-set.ts
@@ -5,11 +5,11 @@ const isDivisible5 = (n: number): boolean => n % 5 === 0;
 
 for (let i=1; i<=100; ++i) {
     const result = DecisionTable.set(
-        [isDivisible5(i), isDivisible3(i),           ],
-        [false,           false,           `${ i }`  ],
-        [false,           true,            'fizz'    ],
-        [true,            false,           'buzz'    ],
-        [true,            true,            'fizzbuzz'],
+        [ isDivisible5(i), isDivisible3(i),            ],
+        [ false,           false,           `${ i }`   ],
+        [ false,           true,            'fizz'     ],
+        [ true,            false,           'buzz'     ],
+        [ true,            true,            'fizzbuzz' ],
     );
 
     console.log(result);
